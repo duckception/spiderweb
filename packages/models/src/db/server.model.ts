978c72b14915365607ffd272d44bd6c7fe0c6d11
Daniel Izdebski <spidiritsu@gmail.com>
2021-01-21 02:16:20 +0100

    Update server model

--- packages/models/src/db/server.model.ts
+++ packages/models/src/db/server.model.ts
@@ -2,12 +2,12 @@ import mongoose, { Schema, Document } from 'mongoose'
 
 interface IServer extends Document {
   name: string,
-  data: string,
+  data: Record<string, unknown>,
 }
 
 const Server: Schema = new Schema({
   name: { type: String, required: true, unique: true },
-  data: { type: String, required: true },
+  data: { type: Object, required: true },
 })
 
 const serverModel = mongoose.model<IServer>('Server', Server)
