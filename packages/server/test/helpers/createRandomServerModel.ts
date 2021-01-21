a868405d4880924ec6b8dafc6690d693da2a8142
Daniel Izdebski <spidiritsu@gmail.com>
2021-01-21 02:25:07 +0100

    Remove tests from server package

--- packages/server/test/helpers/createRandomServerModel.ts
+++
@@ -1,9 +0,0 @@
-import faker from 'faker'
-import Server, { IServer } from '../../src/models/server.model'
-
-export async function createRandomServerModel(): Promise<IServer> {
-  return Server.create({
-    name: faker.name.jobDescriptor(),
-    data: faker.internet.ip(),
-  })
-}
