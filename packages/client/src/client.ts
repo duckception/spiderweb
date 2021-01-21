c361763c6dfcaebea0fa4a252ad531921c4ff33f
Daniel Izdebski <spidiritsu@gmail.com>
2021-01-21 02:20:58 +0100

    Update client to reflect socket changes made to the server

--- packages/client/src/client.ts
+++ packages/client/src/client.ts
@@ -1,14 +1,24 @@
 import { SERVER_URL, PORT } from './config'
 import { io, Socket } from 'socket.io-client'
+import { Operation, Channel } from '@spiderweb/models'
 
 export class SpiderwebClient {
   socket: Socket
 
-  constructor() {
+  constructor(agentName?: string) {
     this.socket = io(`${SERVER_URL}:${PORT}`)
+    console.log(`[client]: Connected to ${SERVER_URL}:${PORT}`)
+    if (agentName) {
+      this.socket.emit('Agent Registration', agentName)
+    }
   }
 
-  listen(modelName: string, callback: (data: any) => void): void {
-    this.socket.on(modelName, callback)
+  listen(channel: Channel, callback: (data: any) => void): void {
+    console.log(`[client]: Listening on channel - ${channel}`)
+    this.socket.on(channel, callback)
+  }
+
+  send(channel: Channel, data: Operation): void {
+    this.socket.emit(channel, data)
   }
 }
